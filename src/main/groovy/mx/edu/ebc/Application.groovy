6cc9fab1525c05393ff82b19191f08cc19ec782d
OmarAp <o.aparicio@ebc.edu.mx>
2021-04-27 17:13:38 -0500

    config initial secirity

--- src/main/groovy/mx/edu/ebc/Application.groovy
+++ src/main/groovy/mx/edu/ebc/Application.groovy
@@ -4,12 +4,29 @@ import io.micronaut.runtime.Micronaut
 import groovy.transform.CompileStatic
 import io.swagger.v3.oas.annotations.*
 import io.swagger.v3.oas.annotations.info.*
+import io.micronaut.openapi.annotation.OpenAPIInclude
+import io.swagger.v3.oas.annotations.tags.Tag
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.security.SecurityScheme
 
 @OpenAPIDefinition(
     info = @Info(
             title = "api-mobile-openid",
             version = "0.0"
     )
+)
+@OpenAPIInclude(
+        classes = [io.micronaut.security.endpoints.LoginController.class, io.micronaut.security.endpoints.LogoutController.class] ,
+        tags = @Tag(name = "Security")
+)
+@OpenAPIInclude(
+        classes = io.micronaut.management.endpoint.env.EnvironmentEndpoint.class,
+        tags = @Tag(name = "Management"),
+        security = @SecurityRequirement(name = "BEARER", scopes = ["ADMIN"])
+)
+@SecurityScheme(
+    name = "basicAuth", type = SecuritySchemeType.HTTP, scheme = "basic"
 )
 @CompileStatic
 class Application {
